342e3c6ebf678fdb80676346cd72df81124848a6
João Victor <danteopzz@hotmail.com>
2022-12-10 18:34:03 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      },
+      960: {
+        slidesPerView: 4,
+        spaceBetween: 50
+      }
+    }
+  })
